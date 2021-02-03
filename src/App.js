2d61d41da0c5a4345a758819c61480bbf47bc598
Tetiana Bronitska <tanya.nest87@gmail.com>
2021-02-03 15:26:22 +0200

    Config

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from './conpoments/Layout/Layout';
+
+export default class App extends Component {
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  state = {};
+
+  render() {
+    return <Layout></Layout>;
+  }
+}
